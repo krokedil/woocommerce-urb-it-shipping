5a1014bfa3b05b7868f39c1871510868bc500320
Ivar Johansson <ivar@webbmekanikern.se>
2016-06-29 17:36:22 +0200

    Continued IMS integration

--- includes/class-ims.php
+++ includes/class-ims.php
@@ -53,12 +53,20 @@
 			protected function ping() {
 				set_time_limit(0);
 				
+				$this->debug();
+				return;
+				
 				if($this->plugin->setting('ims_sync') === 'stocks') {
 					$this->sync_stocks();
 				}
 				elseif($this->plugin->setting('ims_sync') === 'products') {
 					$this->sync_products();
 				}
+			}
+			
+			
+			protected function debug() {
+				var_dump(class_exists('WC_API_Products'));
 			}
 			
 			
@@ -124,6 +132,8 @@
 					}
 				}
 				
+				$this->clear_stock_cache();
+				
 				echo 'Stock level sync done.';
 			}
 			
@@ -182,7 +192,7 @@
 					
 					// Create product
 					if(!isset($sku_to_id[$product->sku])) {
-						// <- Continue here
+						$this->create_product($product);
 					}
 					else {
 						$product_id = &$sku_to_id[$product->sku];
@@ -194,12 +204,178 @@
 						
 						// Update product
 						else {
-							// <- Continue here
+							$this->update_product($sku_to_id[$product->sku], $product);
 						}
 					}
 				}
 				
+				$this->clear_stock_cache();
+				
 				echo 'Product sync done.';
+			}
+			
+			
+			protected function create_product($data) {
+				try {
+					$status = strtolower($data->status);
+					
+					$new_product = array(
+						'post_title'   => wc_clean($data->name),
+						'post_status'  => ($status === 'active' ? 'publish' : 'draft'),
+						'post_type'    => 'product',
+						'post_content' => $data->description,
+						'post_author'  => 0,
+						'post_date_gmt' => $data->created_at,
+						'post_modified_gmt' => $data->updated_at
+					);
+					
+					$new_product = apply_filters('woocommerce_urb_it_ims_create_product', $new_product, $data);
+					
+					// Attempts to create the new product
+					$id = wp_insert_post($new_product, true);
+					
+					// Checks for an error in the product creation
+					if(is_wp_error($id)) {
+						throw new Exception($id->get_error_message());
+					}
+					
+					// Set product type
+					wp_set_object_terms($id, 'simple', 'product_type');
+					
+					$meta_data = array(
+						'backorders' => 'no',
+						'downloadable' => 'no',
+						'featured' => 'no',
+						'height' => '',
+						'length' => '',
+						'manage_stock' => 'yes',
+						'price' => $data->price,
+						'product_attributes' => array(),
+						'product_image_gallery' => '',
+						'product_version' => WC_VERSION,
+						'purchase_note' => '',
+						'regular_price' => $data->price,
+						'sale_price' => '',
+						'sale_price_dates_from' => '',
+						'sale_price_dates_to' => '',
+						'sku' => $data->sku,
+						'sold_individually' => '',
+						'stock' => (is_array($data->stock) && isset($data->stock[0])) ? $data->stock[0]->quantity : 0,
+						'tax_class' => '',
+						'tax_status' => 'taxable',
+						'thumbnail_id' => '',
+						'virtual' => 'no',
+						'visibility' => 'visible',
+						'weight' => '',
+						'width' => '',
+						'urbit_ims_product_id' => $data->id,
+						'urbit_ims_store_id' => $data->store_id
+					);
+					
+					$meta_data['stock_status'] = $meta_data['stock'] ? 'instock' : 'outofstock';
+					
+					$this->update_media($data->media, $id);
+					
+					// Clear product cache
+					wc_delete_product_transients($id);
+					
+					$this->plugin->log('Created product #' . $id);
+				}
+				catch(Exception $e) {
+					$this->plugin->error('IMS: Error while creating product: ' . $e->getMessage());
+				}
+			}
+			
+			
+			protected function update_product($product_id, $data) {
+				try {
+					$status = strtolower($data->status);
+					
+					$update_product = array(
+						'ID' => $product_id,
+						'post_title'   => wc_clean($data->name),
+						'post_status'  => ($status === 'active' ? 'publish' : 'draft'),
+						'post_content' => $data->description,
+						'post_modified_gmt' => $data->updated_at
+					);
+					
+					$update_product = apply_filters('woocommerce_urb_it_ims_update_product', $update_product, $data);
+					
+					// Attempts to create the new product
+					$id = wp_insert_post($update_product, true);
+					
+					// Checks for an error in the product creation
+					if(is_wp_error($id)) {
+						throw new Exception($id->get_error_message());
+					}
+					
+					// Set product type
+					wp_set_object_terms($id, 'simple', 'product_type');
+					
+					$meta_data = array(
+						'price' => $data->price,
+						'product_version' => WC_VERSION,
+						'regular_price' => $data->price,
+						'sku' => $data->sku,
+						'stock' => (is_array($data->stock) && isset($data->stock[0])) ? $data->stock[0]->quantity : 0,
+						'urbit_ims_product_id' => $data->id,
+						'urbit_ims_store_id' => $data->store_id
+					);
+					
+					$meta_data['stock_status'] = $meta_data['stock'] ? 'instock' : 'outofstock';
+					
+					$this->update_media($data->media, $id);
+					
+					// Clear product cache
+					wc_delete_product_transients($id);
+					wp_cache_delete($id, 'post_meta');
+					
+					$this->plugin->log('Updated product #' . $id);
+				}
+				catch(Exception $e) {
+					$this->plugin->error('IMS: Error while updating product: ' . $e->getMessage());
+				}
+			}
+			
+			
+			protected function save_product_meta($product_id = 0, $meta_data = array()) {
+				$meta_data = apply_filters('woocommerce_urb_it_ims_save_product_meta_data', $meta_data, $product_id);
+				
+				foreach($meta_data as $meta_key => $meta_value) {
+					update_post_meta($product_id, '_' . $meta_key, $meta_value);
+				}
+			}
+			
+			
+			protected function update_media($media = array(), $product_id = 0) {
+				global $wpdb;
+				
+				$existing_image_ids = get_post_meta($product_id, '_product_image_gallery', true);
+				
+				if($existing_image_ids) {
+					$existing_image_ids = array_map('absint', explode(',', $existing_image_ids));
+					
+					$query = '
+						SELECT post_id, meta_value
+						FROM ' . $wpdb->postmeta . '
+						WHERE post_id IN (' . implode(', ', $existing_image_ids) . ')
+							AND meta_key = "_urbit_ims_media_id"
+					';
+					
+					$results = $wpdb->get_results($query);
+					
+					foreach($results as $row) {
+						
+					}
+				}
+				
+				
+			}
+			
+			
+			protected function clear_stock_cache() {
+				delete_transient('wc_low_stock_count');
+				delete_transient('wc_outofstock_count');
 			}
 			
 			
