9cb4f4f5eff11bc598455533d9ecdb4f10f9e7f6
Ivar Johansson <ivar@webbmekanikern.se>
2016-08-24 10:01:26 +0200

    Simplified SDK for better flexibility.

--- includes/sdk/UrbRequestIMS.php
+++ includes/sdk/UrbRequestIMS.php
@@ -1,17 +1,43 @@
 <?php
 	class UrbRequestIMS extends UrbRequest {
 		const IMS_PROD_BASE_URL = 'http://ims-api.urb-it.com/v1/';
-		const IMS_STAGE_BASE_URL = 'http://stage-ims-api.urb-it.com/';
+		#const IMS_STAGE_BASE_URL = 'http://stage-ims-api.urb-it.com/';
+		const IMS_STAGE_BASE_URL = 'http://52.31.215.64/v1/';
+		const IMS_DEV_BASE_URL = 'http://52.49.94.47/v1/';
 		
 		
 		public function __construct($storeKey = '', $sharedSecret = '', $stage = false) {
 			parent::__construct($storeKey, $sharedSecret, $stage);
 			
-			$this->baseUrl = ($this->stage ? self::IMS_STAGE_BASE_URL : self::IMS_PROD_BASE_URL);
+			#$this->baseUrl = ($this->stage ? self::IMS_STAGE_BASE_URL : self::IMS_PROD_BASE_URL);
+			$this->baseUrl = self::IMS_DEV_BASE_URL;
 		}
 		
 		
-		public function GetProducts($attr = array()) {
+		public function __call($name, $args = array()) {
+			// Turn "CamelCase" to "camel_case"
+			$underscored_name = ltrim(strtolower(preg_replace('/[A-Z]/', '_$0', $name)), '_');
+			
+			$splitted_name = explode('_', $underscored_name);
+			$http_method = strtoupper(array_shift($splitted_name));
+			$api_method = implode('_', $splitted_name);
+			
+			$this->Call($http_method, $api_method, isset($args[0]) ? $args[0] : array());
+			
+			if($this->httpStatus < 200 || $this->httpStatus > 299) {
+				if($this->httpBody && isset($this->httpBody->developer_message)) {
+					throw new UrbException($this->httpBody->developer_message, $this->httpBody);
+				}
+				else {
+					throw new UrbException('HTTP ' . $this->httpStatus);
+				}
+			}
+			
+			return $this->httpBody;
+		}
+		
+		
+		/*public function GetProducts($attr = array()) {
 			$this->Call('GET', 'products', $attr);
 			
 			if($this->httpStatus !== 200) {
@@ -56,6 +82,6 @@
 			}
 			
 			return $this->httpBody;
-		}
+		}*/
 	}
 ?>
